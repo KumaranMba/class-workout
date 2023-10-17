1f250107df3c59008d2d8eb2ae51c3d9ea76a853
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-17 16:23:24 +0530

    Adding simple console component in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-const App =()=>{
+// const App =()=>{
+//   return(
+//     <p>Hello world!</p>
+//   )
+// }
+
+// export default App;
+
+// create another same method
+
+function App(){
+
+  console.log("Hello from the component");
+
+
   return(
-    <p>Hello world!</p>
+    <div>
+      <p>Hello world</p>
+    </div>
   )
 }
 
-export default App;
-
-/*
-creating command in day2
-*/
+export  default App;
