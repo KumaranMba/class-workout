96da046d7e6fa255a7d47e84393d268210400e94
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-17 18:05:38 +0530

    Adding multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,44 @@
 
 // export default App;
 
+
+
 // create another same method
+// function App(){
+
+//   const now = new Date();
+//   console.log(now);
+//   const a =10;
+//   const b =20;
+ 
+//   return(
+//     <div>
+//       <p>Hello world, it is {now.toString()}</p>
+//       <p> {a} plus {b} is {a+b}</p>
+//     </div>
+//   )
+//   }
+// export  default App;
+
+// Adding multiple components
+
+function Hello(){
+  return(
+    <p>Hello world</p>
+  )
+}
+
 
 function App(){
-
-  const now = new Date();
-  console.log(now);
-  const a =10;
-  const b =20;
- 
   return(
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p> {a} plus {b} is {a+b}</p>
-    </div>
+      <h3>Greetings</h3>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      </div>
   )
-  }
+}
 
-export  default App;
+export default App;
