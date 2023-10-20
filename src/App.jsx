0478abec618dc25482d6c8157d2e9d898ea6d566
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-20 09:08:33 +0530

    creating components and passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,24 @@
-// const App =()=>{
-//   return(
-//     <p>Hello world!</p>
-//   )
+
+// createing components and passing props
+
+// const Welcome =(props)=>{
+//     console.log(props);
+//     return(
+//         <div>
+//             <p>Welcome to my website! {props.name},your age is{props.age}</p>
+//         </div>
+//     )
+// }
+
+// const App = ()=>{
+// const age =25
+//     return(
+//         <div>
+//             <h3>Greetings</h3>
+//             <Welcome name = 'Dinesh' age ={30}/>
+//             <Welcome name = 'Kumaran' age={age} />
+//         </div>
+//     )
 // }
 
 // export default App;
-
-
-
-// create another same method using named function
-// function App(){
-
-//   const now = new Date();
-//   console.log(now);
-//   const a =10;
-//   const b =20;
- 
-//   return(
-//     <div>
-//       <p>Hello world, it is {now.toString()}</p>
-//       <p> {a} plus {b} is {a+b}</p>
-//     </div>
-//   )
-//   }
-// export  default App;
-
-// Adding multiple components
-
-// function Hello(){
-//   return(
-//     <p>Hello world</p>
-//   )
-// }
-
-
-// function App(){
-//   return(
-//     <div>
-//       <h3>Greetings</h3>
-//       <Hello/>
-//       <Hello/>
-//       <Hello/>
-//       <Hello/>
-//       </div>
-//   )
-// }
-
-// export default App;
-
-// Adding properties to function
-
-
-function Hello(name){
-  console.log(name);
-  return(
-    <p>Hello {name.username}</p>
-  )
-}
-function App(){
-
-  let user1 ='dinesh';
-  let user2 ='kumaran';
-  return(
-    <div>
-      <h3>Greetings</h3>
-      <Hello username ={user1}/>
-      <Hello username ={user2}/>
-    </div>
-  )
-}
-
-export default App;
