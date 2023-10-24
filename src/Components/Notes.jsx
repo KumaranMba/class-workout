6531af64ea6cc34ff302485cc6bb1fdf470ebffc
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-24 10:06:31 +0530

    conditional renduring-- first way

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -2,11 +2,18 @@
 import React from 'react'
 
 function Notes({note}) {
-    
+
+  if(note.important){
+    return(
+      <li>{note.content}★</li>
+    )
+  }
+  
   return (
     
     <li>{note.content}</li>
   )
+  
 }
 
 export default Notes;
