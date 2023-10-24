fee786d203c9c28d1ce1f2b5a1aa4288c32046c9
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-24 20:29:44 +0530

    functional component vs class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+//Functional component
+//created as javascript functions
+//it should return the jsx=>Html with javascript embaded.
 
-function App() {
-  return (
-    <div>App</div>
-  )
+// function App() {
+//   return (
+//     <div>Hello World!</div>
+//   )
+// }
+
+// export default App;
+
+//--------------------------------------------------//
+
+// Class component
+import { Component } from "react"; // automatically received from the react library.
+class App extends Component{
+  render(){
+    return(
+      <div>Hello world!</div>
+    )
+  }
 }
 
 export default App;
