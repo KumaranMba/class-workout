fe6c14aab2eefa80104daad2ceb88a3130125d51
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-24 19:25:10 +0530

    Day4 starting point

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,8 @@
-
 import React from 'react'
-import Notes from './Components/Notes';
-import'./style/App.css';
 
-
-function App({notes}) {
-//  const notes = props.notes;
-//   const {notes} =props;
- function handleSaveClick(){
-  console.log('save button clicked');
- }
-
+function App() {
   return (
-    <div>
-        <h1>Notes</h1>
-        <ul>{
-            notes.map(note=>
-            <Notes key={note.id} note={note}/>
-             )
-            }
-        </ul>
-
-        <form >
-          <input/>
-          <button onClick ={
-            () =>console.log("Save button clicked")
-          } >SaveNotes
-          </button>
-        </form>
-    </div>
+    <div>App</div>
   )
 }
 
