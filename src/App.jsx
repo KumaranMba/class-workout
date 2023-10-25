7db07bdddb2a57a3b0d9f975c89a05e1904a7067
kumaranMba <kumarandinesh0411@gmail.com>
2023-10-25 11:13:48 +0530

    class component lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-//rce for creating class component
+// class components lifecycle
+// create(mount,render)---> update(re-render)--->remove(delete) 
 
-import React, { Component } from 'react';
+import { Component } from "react";
 
-class Hello extends Component{
-  render(){
-    // console.log(this.props);
-    const {name} = this.props;
-    return(
-      <p>Hello {name}</p>
-    )
+
+class App extends Component{
+
+  // component did mount : called after the component has been render to the DOM.
+  
+  componentDidMount(){
+    console.log('component is mounted');
   }
-}
 
+  //called immediately after the component is updated in the DOM
+  // called on state of prop changes.
+  componentDidUpdate(){
+    consolelog('component state update')
+    }
 
+  componentWillUnmount(){
+    //invoked just before the comonent is removed from the dom
+    //releasing memory or resources
+    //detaching the events
+    //cancelling timers
+  }
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Hello name ='Dinesh!'/>
-      </div>
-    )
+  render(){
+   return(
+    <div>
+      <p>Hello world!</p>
+    </div>
+   )
+    
   }
 }
 
 export default App;
 
+/*components lifecycle: The different states/stages a component takes from getting created and render in the Dom 
+to getting removed from the Dom */
