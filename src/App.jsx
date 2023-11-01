270995e2e2b6a5834fb9c76e355e008e1dfdb2ce
kumaranMba <kumarandinesh0411@gmail.com>
2023-11-01 12:00:54 +0530

    Passing data from child component to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
+// Passing data from child to parent.
+
 import React from 'react';
 import { useState } from 'react';
 
-//child component 
-// Receive the child as a data props.
-function Hello({data}){
+// child component
+function Childcomponent({sentdatatoparent}){
+  // passing data from the child to parent component.
+  let message = `Hello parent`;
+  const dataToSentToParent=()=>{
+    sentdatatoparent(message);
+  }
   return(
-    <div>Hello{data}</div>
+    <div>
+      <button onClick={dataToSentToParent}>sendDataToTheParent</button>
+      </div>
   )
+
 }
+
 
 // parent component
 function App() {
+ //Define a state to store the child data in the parent component.
+ const [datafromchild,setDatafromchild] = useState('');
+ 
+ const handledatafromchild =(message)=>{
+  setDatafromchild(message);
+ }
 
-  const [data,setData] = useState(`dinesh`);
   return (
     <div>
-      <Hello data={data}/>
+      <div><p>Data from child:{datafromchild}</p></div>
+      <div><Childcomponent sentdatatoparent={handledatafromchild}/></div>
     </div>
   )
 }
