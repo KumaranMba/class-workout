6a40e9451985affa7b9b1b86045e87f725727e32
kumaranMba <kumarandinesh0411@gmail.com>
2023-11-01 08:53:12 +0530

    Passing data props from parent to child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
 
-
-/*props drilling: props drilling occurs when you need to pass down the data 
-through multiple nested components to reach the ones that require the data. */
-
-import React, { useState } from 'react';
-
-function Grandchildcomponent({data}){
-  console.log(`GrandChild received from parentdata:${data}`)
- 
+//child component 
+// Receive the child as a data props.
+function Hello({data}){
   return(
-    <div>
-      <h2>Grandchildcomponent</h2>
-
-    </div>
-
+    <div>Hello{data}</div>
   )
 }
 
-function Childcomponent({data}){
-  console.log(data);
-  return(
-    <div>
-   <h2>Child component</h2>
-    <Grandchildcomponent data ={data}/>
-    </div>
-    )
-}
-
+// parent component
 function App() {
-   const parentdata =` Hello from the parent`;
-  //const[value,setValue]=useState(` Hello from the parent`);// useing useState method.
+
+  const [data,setData] = useState(`dinesh`);
   return (
     <div>
-      <h2>Parent component</h2>
-        <Childcomponent data={parentdata}/>
-
+      <Hello data={data}/>
     </div>
   )
 }
